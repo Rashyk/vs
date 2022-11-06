60edd83efe9f87d47c4cd320ee4313d26033a596
mihailrash <mihailrash@gmail.com>
2022-11-06 21:18:51 +0200

    u

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import jsonData from "./data.json";
 
 function App() {
+
   console.log(jsonData)
+
   let menu = jsonData.menu.popup.menuitem
+
+  let text = 'default'
+
   return (
     <div className="App">
         {
             menu.map( (menuItem, key) => <li>{menuItem.value}</li>)
         }
         //зробить для селекта
+        <h3>{text}</h3>
+
 
         //
     </div>
